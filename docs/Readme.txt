cc2469d88ca81d1325832a0fd984e31db3275999
pchenthx <pradeepx.chenthati@intel.com>
2014-07-29 14:38:52 -0700

    Initial project setup
    Signed-off-by: pchenthx <pradeepx.chenthati@intel.com>

---
+++ docs/Readme.txt
@@ -0,0 +1,1 @@
+Documentation shall go here
---
+++ src/iotkit.c
@@ -0,0 +1,28 @@
+/*
+ * iotkit 'C' Library to communicate with IoT Cloud via REST APIs
+ * Copyright (c) 2014, Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ */
+
+/**
+ * @file iotkit.c
+ * @brief Implementation of iotkit library
+ *
+ * Provides features for communication with IoT Cloud server
+ */
+
+#include "iotkit.h"
+
+#if DEBUG
+void main() {
+    puts("Iotkit Library to communicate with IoT Cloud via REST APIs");
+}
+#endif
