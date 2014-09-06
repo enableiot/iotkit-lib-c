588ac507f8bec48f780e2f119327cd17e0be3a19
PradeepX Chenthati <pradeepx.chenthati@intel.com>
2014-09-05 17:36:40 -0700

    Passing response variable to receive the response from doHttpGet operation

--- src/account_management.c
+++ src/account_management.c
@@ -55,7 +55,7 @@ char *getAccountInformation() { // TODO: this should taken data account id as a 
     appendHttpHeader(&headers, HEADER_AUTHORIZATION, getConfigAuthorizationToken());
 
     if(prepareUrl(&url, configurations.base_url, configurations.get_account_information)){
-        doHttpGet(url, headers);
+        doHttpGet(url, headers, &response);
 
         return response;
     }
@@ -73,7 +73,7 @@ char *getAccountActivationCode() { // TODO: this should taken data account id as
 
     if(prepareUrl(&url, configurations.base_url, configurations.get_account_activation_code)) {
 
-        doHttpGet(url, headers);
+        doHttpGet(url, headers, &response);
 
         return response;
     }
